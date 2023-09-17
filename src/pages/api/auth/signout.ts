5b76a42b80b79a8a52f2c3a1d904824def45f8e5
jianmin-chen <jianmin0chen@gmail.com>
2023-09-17 19:13:55 -0400

    Serverless function limit shit

--- src/pages/api/auth/signout.ts
+++
@@ -1,9 +0,0 @@
-import type { APIRoute } from 'astro'
-
-export const GET: APIRoute = async ({ redirect, cookies }) => {
-  cookies.delete('session', {
-    path: '/'
-  })
-
-  return redirect('/')
-}
